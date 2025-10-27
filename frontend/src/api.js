192934da2b7e74e2743f32d22aad52a24d2e9640
Tech-WizardsLab <z2htoken@hotmail.com>
2025-10-27 17:58:51 +0000

    �� Use direct Render URL for backend API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://sales-tracker-ul4y.onrender.com/api',
+  baseURL: 'https://sales-tracker-ul4y.onrender.com/api',
 });
 
 export default API;
